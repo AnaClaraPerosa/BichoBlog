42a9529ac29e399a27f2aa3649bb52cd01306f5e
Ana Clara <anaclaraperosa@gmail.com>
2025-01-06 22:51:46 -0300

    mudanças

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 import "@hotwired/turbo-rails"
 import "controllers"
-import "bootstrap"
+import "bootstrap";
+import "../assets/stylesheets/application.scss";
 import Rails from "@rails/ujs";
 Rails.start();
